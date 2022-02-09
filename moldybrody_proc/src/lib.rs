d5b5b51c17a602ebcf2cd9730f04ef604b9377ac
key262yek <key262yek@gmail.com>
2022-02-09 11:51:26 +0900

    Implement State derive macro
    State macro는 Mass, Charge, State, HasVelocity와 같은 통상적으로 변수명이 크게 달라지지 않는 field에 관한 trait을 자동으로 implement하는 macro입니다. 이것만으로 시뮬레이션 단계에서 임의로 정의한 state struct로 하여금 moldybrody의 기능들을 이용할 수 있습니다.

--- moldybrody_proc/src/lib.rs
+++ moldybrody_proc/src/lib.rs
@@ -1,72 +1,10 @@
 extern crate proc_macro;
-use proc_macro::TokenStream;
+use proc_macro::{Span, TokenStream, TokenTree};
 use proc_quote::{quote, quote_spanned};
-use syn::spanned::Spanned;
-use syn::{parse_macro_input, DeriveInput, Data, Fields, Index};
+use syn::{parse_macro_input, DeriveInput, Data, Fields, Ident, Type};
+use std::iter;
 
 
-/// Derive macros autocomplete the trait Topology.
-///
-/// Topology trait을 autocomplete 해주는 매크로이다.
-/// Topology trait은 대응하는 Point generic type이 특정되어야하는데,
-/// 이를 위해서 PointName이라는 attributes를 받는다.
-#[proc_macro_derive(Topology, attributes(PointName))]
-pub fn derive_topology(item: TokenStream) -> TokenStream {
-    let x = parse_macro_input!(item as DeriveInput);
-
-    let name = x.ident;
-    let point_name : syn::Ident = x.attrs[0].parse_args().unwrap();
-
-    let tokens = match x.data{
-        Data::Struct(ref data) => {
-            match data.fields{
-                Fields::Unnamed(ref fields) =>{
-                    let recurse = fields.unnamed.iter().enumerate().map(|(i, f)| {
-                        let index = Index::from(i);
-                        quote_spanned! {f.span()=>
-                            self.#index.check_move(&pos.#index, &movement.#index)
-                        }
-                    });
-                    quote! {
-                        impl Topology<#point_name> for #name{
-                            fn check_move(&self, pos : &#point_name, movement : &#point_name) -> bool{
-                                false #(|| #recurse)*
-                            }
-                        }
-                    }
-                },
-                _ => unimplemented!(),
-            }
-        },
-        Data::Enum(_) |
-        Data::Union(_) => unimplemented!(),
-    };
-    // panic!("{}", tokens.to_string());
-    tokens.into()
-}
-
-/// Declarative macro to generate derive macro
-#[allow(unused_macros)]
-macro_rules! simple_trait{
-    ($trait_name:ident, $func_name:ident) => {
-        #[proc_macro_derive($trait_name)]
-        pub fn $func_name(item : TokenStream) -> TokenStream{
-            let x = parse_macro_input!(item as DeriveInput);
-
-            let struct_name = x.ident;
-            let (impl_generics, ty_generics, where_clause) = x.generics.split_for_impl();
-            let tokens = quote!{
-                impl #impl_generics $trait_name for #struct_name #ty_generics #where_clause {}
-            };
-            tokens.into()
-        }
-    }
-}
-
-simple_trait!(Point, derive_point);
-simple_trait!(Force, derive_force);
-simple_trait!(Displacement, derive_disp);
-simple_trait!(State, derive_state);
 
 #[proc_macro_derive(Mass)]
 pub fn derive_mass(item : TokenStream) -> TokenStream{
@@ -74,14 +12,280 @@ pub fn derive_mass(item : TokenStream) -> TokenStream{
 
     let struct_name = x.ident;
     let (impl_generics, ty_generics, where_clause) = x.generics.split_for_impl();
-    let tokens = quote!{
-        impl #impl_generics Mass for #struct_name #ty_generics #where_clause {
-            fn mass(&self) -> f64{
-                self.mass
+
+    let names = match x.data{
+        Data::Struct(ds) => {
+            match ds.fields{
+                Fields::Named(n) => {
+                    n.named
+                },
+                _ => {
+                    return err(Span::call_site(), "expected named fields, found unnamed or unit");
+                }
+            }
+        },
+        _ => {
+            return err(Span::call_site(), "expected struct, found enum or union");
+        },
+    };
+
+    let mass_ident = Ident::new("mass", proc_macro2::Span::call_site());
+    for f in names.iter(){
+        let ident = f.ident.clone().unwrap();
+        if ident == mass_ident{
+            let ty = f.ty.clone();
+            let tokens = quote!{
+                impl #impl_generics Mass<#ty> for #struct_name #ty_generics #where_clause {
+                    fn mass(&self) -> #ty{
+                        self.mass
+                    }
+                }
+            };
+            return tokens.into();
+        }
+    }
+    return err(Span::call_site(), "There is no fields of name 'mass' in struct");
+}
+
+#[proc_macro_derive(Charge)]
+pub fn derive_charge(item : TokenStream) -> TokenStream{
+    let x = parse_macro_input!(item as DeriveInput);
+
+    let struct_name = x.ident;
+    let (impl_generics, ty_generics, where_clause) = x.generics.split_for_impl();
+
+    let names = match x.data{
+        Data::Struct(ds) => {
+            match ds.fields{
+                Fields::Named(n) => {
+                    n.named
+                },
+                _ => {
+                    return err(Span::call_site(), "expected named fields, found unnamed or unit");
+                }
+            }
+        },
+        _ => {
+            return err(Span::call_site(), "expected struct, found enum or union");
+        },
+    };
+
+    let mass_ident = Ident::new("charge", proc_macro2::Span::call_site());
+    for f in names.iter(){
+        let ident = f.ident.clone().unwrap();
+        if ident == mass_ident{
+            let ty = f.ty.clone();
+            let tokens = quote!{
+                impl #impl_generics Charge<#ty> for #struct_name #ty_generics #where_clause {
+                    fn charge(&self) -> #ty{
+                        self.charge
+                    }
+                }
+            };
+            return tokens.into();
+        }
+    }
+    return err(Span::call_site(), "There is no fields of name 'charge' in struct");
+}
+
+
+#[proc_macro_derive(State)]
+pub fn derive_state(item : TokenStream) -> TokenStream{
+    fn from_string(name : &str) -> Ident{
+        Ident::new(name, proc_macro2::Span::call_site())
+    }
+    let x = parse_macro_input!(item as DeriveInput);
+
+    let struct_name = x.ident;
+    let (impl_generics, ty_generics, where_clause) = x.generics.split_for_impl();
+
+    let names = match x.data{
+        Data::Struct(ds) => {
+            match ds.fields{
+                Fields::Named(n) => {
+                    n.named
+                },
+                _ => {
+                    return err(Span::call_site(), "expected named fields, found unnamed or unit");
+                }
+            }
+        },
+        _ => {
+            return err(Span::call_site(), "expected struct, found enum or union");
+        },
+    };
+
+    let mut ty_pos : Option<Type> = None;
+    let mut ty_vel : Option<Type> = None;
+
+    let mut token = quote!{};
+
+    for f in names.iter(){
+        let ident = f.ident.clone().unwrap();
+        if ident == from_string("mass"){
+            let ty = f.ty.clone();
+
+
+            token = quote!{
+                #token
+
+                impl #impl_generics Mass<#ty> for #struct_name #ty_generics #where_clause {
+                    fn mass(&self) -> #ty{
+                        self.mass
+                    }
+                }
+            };
+        } else if ident == from_string("charge"){
+            let ty = f.ty.clone();
+
+
+            token = quote!{
+                #token
+
+                impl #impl_generics Charge<#ty> for #struct_name #ty_generics #where_clause {
+                    fn charge(&self) -> #ty{
+                        self.charge
+                    }
+                }
+            };
+        } else if ident == from_string("pos"){
+            ty_pos = Some(f.ty.clone());
+        } else if ident == from_string("vel"){
+            ty_vel = Some(f.ty.clone());
+        }
+    }
+
+    match ty_pos{
+        Some(p) => {
+            match ty_vel{
+                Some(v) => {
+                    token = quote!{
+                        #token
+
+                        impl #impl_generics State for #struct_name #ty_generics #where_clause {
+                            type Movement = (#p, #v);
+                            type Position = #p;
+
+                            fn pos(&self) -> &Self::Position{
+                                &self.pos
+                            }
+                            fn pos_mut(&mut self) -> &mut Self::Position{
+                                &mut self.pos
+                            }
+
+                            fn disp<'a>(&self, movement : &'a Self::Movement) -> &'a Self::Position{
+                                &movement.0
+                            }
+
+                            fn renew_state(&mut self, movement : &Self::Movement){
+                                self.pos += &movement.0;
+                                self.vel += &movement.1;
+                            }
+                        }
+
+                        impl #impl_generics HasVelocity for #struct_name #ty_generics #where_clause {
+                            fn vel(&self) -> &<Self as State>::Position {
+                                &self.vel
+                            }
+
+                            fn vel_mut(&mut self) -> &mut <Self as State>::Position {
+                                &mut self.vel
+                            }
+                        }
+                    };
+                },
+                None => {
+                    token = quote!{
+                        #token
+
+                        impl #impl_generics State for #struct_name #ty_generics #where_clause {
+                            type Movement = #p;
+                            type Position = #p;
+
+                            fn pos(&self) -> &Self::Position{
+                                &self.pos
+                            }
+                            fn pos_mut(&mut self) -> &mut Self::Position{
+                                &mut self.pos
+                            }
+
+                            fn disp<'a>(&self, movement : &'a Self::Movement) -> &'a Self::Position{
+                                movement
+                            }
+
+                            fn renew_state(&mut self, movement : &Self::Movement){
+                                self.pos += movement;
+                            }
+                        }
+                    };
+                }
+            }
+        },
+        None => {
+            return err(Span::call_site(), "There is no fields of name 'pos' in struct. State derive macro only work for struct has fields pos, (optional) vel, mass, charge");
+        }
+    }
+    return token.into();
+}
+
+
+
+
+
+/// Expands into multiple `iterator.next(),` statements. E.g.
+/// `many_greetings!(3);` will expand into three `println`s.
+#[proc_macro]
+pub fn iter_to_array(input: TokenStream) -> TokenStream {
+    let tokens = input.into_iter().collect::<Vec<_>>();
+
+    // Make sure at least one token is provided.
+    if tokens.is_empty() {
+        return err(Span::call_site(), "expected integer, found no input");
+    }
+
+    // Make sure we don't have too many tokens.
+    // first : identifier
+    // second : comma
+    // third : count
+    if tokens.len() > 3 {
+        return err(tokens[3].span(), "unexpected second token");
+    }
+
+    // Get iterator name
+
+
+    // Get the number from our token.
+    let count = match &tokens[1] {
+        TokenTree::Literal(lit) => {
+            // Unfortunately, `Literal` doesn't have nice methods right now, so
+            // the easiest way for us to get an integer out of it is to convert
+            // it into string and parse it again.
+            if let Ok(count) = lit.to_string().parse::<usize>() {
+                count
+            } else {
+                let msg = format!("expected unsigned integer, found `{}`", lit);
+                return err(lit.span(), msg);
             }
         }
+        other => {
+            let msg = format!("expected integer literal, found `{}`", other);
+            return err(other.span(), msg);
+        }
     };
-    tokens.into()
+
+    // Return multiple `println` statements.
+    iter::repeat(quote! { ,  })
+        .map(TokenStream::from)
+        .take(count)
+        .collect()
+}
+
+/// Report an error with the given `span` and message.
+fn err(span: Span, msg: impl Into<String>) -> TokenStream {
+    let msg = msg.into();
+    quote_spanned!(span.into()=> {
+        compile_error!(#msg);
+    }).into()
 }
 
 
