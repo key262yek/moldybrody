578fa1f0652964960d1a3cb3e287eb47431c9570
Myeongseon Choi <key262yek@gmail.com>
2022-08-03 09:25:17 +0900

    Update prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,52 +1,48 @@
-
-pub use moldybrody_proc::{State};
+pub use moldybrody_proc::State;
 
 #[allow(unused_imports)]
 pub use crate::{
-    // system::{Point, Topology},
-    // force::{GlobalPotential, BimolecularInteraction, RandomForce},
-    // approx::{Approximation, TimeIterator, TimeDiffIterator},
+    approx::{
+        time::{ConstStep, ExponentialStep},
+        ApproxNewton, TimeIterator,
+    },
+    boundary::{
+        plane::{
+            Cube, Direction, Parallelogram, Plane, PlanePair, SimpleBox, SimplePlane,
+            SimplePlanePair,
+        },
+        sphere::{Sphere, TaxiSphere},
+        AfterMove, BCspecies, BoundaryCondition, FloatBoundary, IntBoundary, NonPeriodic,
+        OverdampedBoundaryCondition, OverdampedSystem, Periodic, System,
+    },
+    state::{Charge, HasVelocity, Mass, State},
+    vector::{
+        basic::{Map, Zeros},
+        product::{Cross, Distance, Dot, InnerProduct, Norm},
+        Cartessian, Cartessian1D, Cartessian2D, Cartessian3D, Cartessian4D, CartessianND,
+    },
 };
 
 #[allow(unused_imports)]
-pub(crate) use crate::{
-    // system::{
-    //     point::{Cartessian1D, Cartessian2D, Cartessian3D, Cartessian4D, CartessianND,
-    //             Node, Edge},
-    //     topology::{ContinuousTopology, LatticeTopology, NetworkTopology},
-    // },
-    // force::{
-    //     global::{ConstantGravity, AccGravity, Mass},
-    //     bimolecular::{},
-    //     random::{GaussianNoise}
-    // },
-    // approx::{
-    //     state::{GeneralState, OverdampedState},
-    //     time::{ConstStep, ExponentialStep},
-    // },
-    error::{Result, Error, ErrorCode},
-};
+pub(crate) use crate::error::{Error, ErrorCode, Result};
 
 #[allow(unused_imports)]
 pub(crate) use rand_pcg::Pcg64;
 
 #[allow(unused_imports)]
 pub(crate) use std::{
-    ops::{Add, Mul, Sub},
-    iter::Iterator,
-    fmt::{self, Display, Formatter, LowerExp, Write},
-    str::FromStr,
-    io::{prelude::*, self, Lines, BufReader, BufWriter},
-    fs::{self, File},
-    convert::AsRef,
-    path::Path,
-    hash::{Hash, Hasher},
     collections::HashMap,
+    convert::AsRef,
     default::Default,
+    fmt::{self, Display, Formatter, LowerExp, Write},
+    fs::{self, File},
+    hash::{Hash, Hasher},
+    io::{self, prelude::*, BufReader, BufWriter, Lines},
+    iter::Iterator,
+    ops::{Add, Mul, Sub},
+    path::Path,
+    str::FromStr,
 };
 
 #[allow(unused_imports)]
 pub(crate) use std::f64::consts::PI;
-
-
-
