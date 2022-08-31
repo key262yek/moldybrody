8f210e98126a1818b255c9cb17dd3b603c51b7b3
Myeongseon Choi <key262yek@gmail.com>
2022-08-31 18:30:02 +0900

    Implement Integrator builder and tableau builder

--- src/approx/mod.rs
+++ src/approx/mod.rs
@@ -12,23 +12,214 @@ use crate::state::State;
 use crate::vector::Scalar;
 use crate::vector::Vector;
 // use std::ops::{Index, IndexMut};
+use approx::AbsDiffEq;
+
+pub struct ButcherTableau<T>{
+    aij : Vec<Vec<T>>,
+    bi : Vec<T>,
+    ci : Vec<T>,
+}
+
+enum NewtonButcherTableauBuilder<T>{
+    Euler,
+    MidPoint,
+    Ssprk3,
+    Classic,
+    Heun(usize),
+    Ralston(usize),
+    Generic(usize, T),
+}
+
+impl<T> NewtonButcherTableauBuilder<T>{
+    fn new(name : Option<&str>, order : Option<usize>, alpha : Option<T>) -> Self{
+        match name{
+            Some("Euler") => NewtonButcherTableauBuilder::Euler,
+            Some("MidPoint") => NewtonButcherTableauBuilder::MidPoint,
+            Some("SSPRK3") => NewtonButcherTableauBuilder::Ssprk3,
+            Some("Classic") => NewtonButcherTableauBuilder::Classic,
+            Some("Heun") => {
+                match order {
+                    Some(v) => NewtonButcherTableauBuilder::Heun(v),
+                    None => panic!("Heun method requires order attribute."),
+                }
+            },
+            Some("Ralston") => {
+                match order {
+                    Some(v) => NewtonButcherTableauBuilder::Ralston(v),
+                    None => panic!("Ralston method requires order attribute."),
+                }
+            },
+            Some("Generic") => {
+                match (order, alpha) {
+                    (Some(v), Some(a)) => NewtonButcherTableauBuilder::Generic(v, a),
+                    (Some(_), None) => panic!("Generic Runge-Kutta method requires parameter alpha in [0, 1]"),
+                    (None, _) => panic!("Generic Runge-Kutta method requires order"),
+                }
+            }
+            None | Some(_) => panic!("Butcher Tableau builder requires valid name of tableau. ex) Euler, Midpoint, SSPRK3, Classic, Heun, Ralston, Generic"),
+        }
+    }
+}
+
+macro_rules! impl_bt_builder{
+    ($ty : ident) => {
+        impl NewtonButcherTableauBuilder<$ty> {
+            fn build(self) -> ButcherTableau<$ty>{
+                match self{
+                    NewtonButcherTableauBuilder::Euler => {
+                        ButcherTableau{
+                            aij : vec![vec![0.]],
+                            bi : vec![1.],
+                            ci : vec![0.],
+                        }
+                    },
+                    NewtonButcherTableauBuilder::MidPoint => {
+                        ButcherTableau{
+                            aij : vec![vec![0., 0.], vec![0.5, 0.]],
+                            bi : vec![0., 1.],
+                            ci : vec![0., 0.5],
+                        }
+                    },
+                    NewtonButcherTableauBuilder::Ssprk3 => {
+                        let t = 1. / 6.;
+                        ButcherTableau{
+                            aij : vec![vec![0., 0., 0.], vec![1., 0., 0.], vec![0.25, 0.25, 0.]],
+                            bi : vec![t, t, 4. * t],
+                            ci : vec![0., 1., 0.5],
+                        }    
+                    },
+                    NewtonButcherTableauBuilder::Classic => {
+                        let t = 1. / 6.;
+                        ButcherTableau{
+                            aij : vec![vec![0., 0., 0., 0.], vec![0.5, 0., 0., 0.], vec![0., 0.5, 0., 0.], vec![0., 0., 1., 0.]],
+                            bi : vec![t, 2. * t, 2. * t, t],
+                            ci : vec![0., 0.5, 0.5, 1.],
+                        } 
+                    },
+                    NewtonButcherTableauBuilder::Heun(order) => {
+                        match order {
+                            2 => {
+                                ButcherTableau{
+                                    aij : vec![vec![0., 0.], vec![1., 0.]],
+                                    bi : vec![0.5, 0.5],
+                                    ci : vec![0., 1.],
+                                }
+                            },
+                            3 => {
+                                let t = 1. / 3.;
+                                ButcherTableau{
+                                    aij : vec![vec![0., 0., 0.], vec![t, 0., 0.], vec![0., 2. * t, 0.]],
+                                    bi : vec![0.25, 0., 0.75],
+                                    ci : vec![0., t, 2. * t],
+                                }
+                            }, 
+                            _ => {
+                                panic!("Heun methods exist only for 2nd and 3rd order");
+                            }
+                        }
+                    },
+                    NewtonButcherTableauBuilder::Ralston(order) => {
+                        match order {
+                            2 => {
+                                ButcherTableau{
+                                    aij : vec![vec![0., 0.], vec![2. / 3., 0.]],
+                                    bi : vec![0.25, 0.75],
+                                    ci : vec![0., 2. / 3.],
+                                }
+                            },
+                            3 => {
+                                let t = 1. / 9.;
+                                ButcherTableau{
+                                    aij : vec![vec![0., 0., 0.], vec![0.5, 0., 0.], vec![0., 0.75, 0.]],
+                                    bi : vec![2. * t, 3. * t, 4. * t],
+                                    ci : vec![0., 0.5, 0.75],
+                                }    
+                            }, 
+                            _ => {
+                                panic!("Raltson methods exist only for 2nd and 3rd order");
+                            }
+                        }
+                    },
+                    NewtonButcherTableauBuilder::Generic(order, alpha) => {
+                        match order {
+                            2 => {
+                                let t = 1. / (2. * alpha);
+                                ButcherTableau{
+                                    aij : vec![vec![0., 0.], vec![alpha, 0.]],
+                                    bi : vec![1. - t, t],
+                                    ci : vec![0., alpha],
+                                }
+                            },
+                            3 => {
+                                if alpha.abs_diff_eq(&0., 1e-5) || alpha.abs_diff_eq(&(2. / 3.), 1e-5) || alpha.abs_diff_eq(&1., 1e-5){
+                                    panic!("Generic 3rd order Runge-Kutta method cannot be made with alpha = 0, 2/3, 1");
+                                }
+                                let t = (1. - alpha) / (3. * alpha - 2.);
+                                ButcherTableau{
+                                    aij : vec![vec![0., 0., 0.], vec![alpha, 0., 0.], vec![1. + t / alpha, - t / alpha, 0.]],
+                                    bi : vec![0.5 - 1. / (6. * alpha), 1. / (6. * alpha * (1. - alpha)), - t / 6.],
+                                    ci : vec![0., alpha, 1.],
+                                }
+                            }, 
+                            _ => {
+                                panic!("Generic Runge-Kutta tableau exist only for 2nd and 3rd order");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+impl_bt_builder!(f32);
+impl_bt_builder!(f64);
+
+
 
 /// Approximation method
 ///
 /// System에 따라 정해지는 State와 Force, Time Iterator를 인자로 해서 State를 renewal하는 approximation method를 지칭합니다.
 /// 같은 시스템에서도 approximation order에 따라 그 형태가 달라질 수 있습니다.
-pub trait ApproxNewton<'a, P, T>: State<Movement = (P, P), Position = P>
-where
-    P: Vector<Item = T>,
-    T: Scalar,
-{
-    /// Approximation by euler method
-    fn euler(&'a self, force: &'a P, dt: T) -> (P, P);
+pub struct NewtonRKIntegratorBuilder<T>{
+    name_tableau : Option<&'static str>,
+    order_tableau : Option<usize>,
+    alpha_tableau : Option<T>,
+}
+impl<T : Copy> NewtonRKIntegratorBuilder<T>{
+    pub fn new() -> Self{
+        Self{
+            name_tableau : None,
+            order_tableau : None,
+            alpha_tableau : None,
+        }
+    }
 
-    /// Approximation for constant speed particle
-    fn const_speed(&'a self, dt: T) -> (P, P);
+    pub fn name_tableau(mut self, name : &'static str) -> Self{
+        self.name_tableau = Some(name);
+        self
+    }
+
+    pub fn order_tableau(mut self, order : usize) -> Self{
+        self.order_tableau = Some(order);
+        self
+    }
+
+    pub fn alpha_tableau(mut self, alpha : T) -> Self{
+        self.alpha_tableau = Some(alpha);
+        self
+    }
+
+    pub fn build() -> NewtonRKIntegrator{
+        unimplemented!();
+    }
 }
 
+pub struct NewtonRKIntegrator(Box<dyn NewtonRKIntegratorTrait>);
+trait NewtonRKIntegratorTrait{
+    fn iterate(&self){
+        unimplemented!();
+    }
+}
 
 
 pub trait ApproxOverdampedLangevin<'a, P, T>: State<Movement = P, Position = P>
--- src/approx/newton.rs
+++ src/approx/newton.rs
@@ -1,4 +1,4 @@
-use crate::approx::ApproxNewton;
+
 use crate::prelude::Mul;
 use crate::vector::basic::Zeros;
 use std::ops::Add;
@@ -10,36 +10,3 @@ use crate::approx::Vector;
 use crate::state::HasVelocity;
 use crate::state::Mass;
 
-macro_rules! impl_newton_float {
-    ($ty : ident) => {
-        impl<'a, S, P> ApproxNewton<'a, P, $ty> for S
-        where
-            S: State<Movement = (P, P), Position = P> + HasVelocity + Mass<$ty>,
-            P: Vector<Item = $ty>
-                + Add<Output = P>
-                + Clone
-                + AddAssign<&'a P>
-                + AddAssign<P>
-                + MulAssign<$ty>
-                + Zeros
-                + Mul<$ty, Output = P>
-                + 'a,
-            &'a P: Mul<$ty, Output = P> + Add<P, Output = P>,
-        {
-            fn euler(&'a self, force: &'a P, dt: $ty) -> (P, P) {
-                let dv = force * (dt / self.mass());
-                let dx = (self.vel() + dv.clone() * 0.5 as $ty) * dt;
-                return (dx, dv);
-            }
-
-            fn const_speed(&'a self, dt: $ty) -> (P, P) {
-                let dx = self.vel() * dt;
-                let dv = P::zero_with_length(self.vel().dim());
-                return (dx, dv);
-            }
-        }
-    };
-}
-
-impl_newton_float!(f32);
-impl_newton_float!(f64);
