b1b284b438874774bd24c8959777339a22a8d7e4
Myeongseon Choi <key262yek@gmail.com>
2022-09-01 09:06:37 +0900

    split tableau module

--- src/approx/mod.rs
+++ src/approx/mod.rs
@@ -12,168 +12,6 @@ use crate::state::State;
 use crate::vector::Scalar;
 use crate::vector::Vector;
 // use std::ops::{Index, IndexMut};
-use approx::AbsDiffEq;
-
-pub struct ButcherTableau<T>{
-    aij : Vec<Vec<T>>,
-    bi : Vec<T>,
-    ci : Vec<T>,
-}
-
-enum NewtonButcherTableauBuilder<T>{
-    Euler,
-    MidPoint,
-    Ssprk3,
-    Classic,
-    Heun(usize),
-    Ralston(usize),
-    Generic(usize, T),
-}
-
-impl<T> NewtonButcherTableauBuilder<T>{
-    fn new(name : Option<&str>, order : Option<usize>, alpha : Option<T>) -> Self{
-        match name{
-            Some("Euler") => NewtonButcherTableauBuilder::Euler,
-            Some("MidPoint") => NewtonButcherTableauBuilder::MidPoint,
-            Some("SSPRK3") => NewtonButcherTableauBuilder::Ssprk3,
-            Some("Classic") => NewtonButcherTableauBuilder::Classic,
-            Some("Heun") => {
-                match order {
-                    Some(v) => NewtonButcherTableauBuilder::Heun(v),
-                    None => panic!("Heun method requires order attribute."),
-                }
-            },
-            Some("Ralston") => {
-                match order {
-                    Some(v) => NewtonButcherTableauBuilder::Ralston(v),
-                    None => panic!("Ralston method requires order attribute."),
-                }
-            },
-            Some("Generic") => {
-                match (order, alpha) {
-                    (Some(v), Some(a)) => NewtonButcherTableauBuilder::Generic(v, a),
-                    (Some(_), None) => panic!("Generic Runge-Kutta method requires parameter alpha in [0, 1]"),
-                    (None, _) => panic!("Generic Runge-Kutta method requires order"),
-                }
-            }
-            None | Some(_) => panic!("Butcher Tableau builder requires valid name of tableau. ex) Euler, Midpoint, SSPRK3, Classic, Heun, Ralston, Generic"),
-        }
-    }
-}
-
-macro_rules! impl_bt_builder{
-    ($ty : ident) => {
-        impl NewtonButcherTableauBuilder<$ty> {
-            fn build(self) -> ButcherTableau<$ty>{
-                match self{
-                    NewtonButcherTableauBuilder::Euler => {
-                        ButcherTableau{
-                            aij : vec![vec![0.]],
-                            bi : vec![1.],
-                            ci : vec![0.],
-                        }
-                    },
-                    NewtonButcherTableauBuilder::MidPoint => {
-                        ButcherTableau{
-                            aij : vec![vec![0., 0.], vec![0.5, 0.]],
-                            bi : vec![0., 1.],
-                            ci : vec![0., 0.5],
-                        }
-                    },
-                    NewtonButcherTableauBuilder::Ssprk3 => {
-                        let t = 1. / 6.;
-                        ButcherTableau{
-                            aij : vec![vec![0., 0., 0.], vec![1., 0., 0.], vec![0.25, 0.25, 0.]],
-                            bi : vec![t, t, 4. * t],
-                            ci : vec![0., 1., 0.5],
-                        }    
-                    },
-                    NewtonButcherTableauBuilder::Classic => {
-                        let t = 1. / 6.;
-                        ButcherTableau{
-                            aij : vec![vec![0., 0., 0., 0.], vec![0.5, 0., 0., 0.], vec![0., 0.5, 0., 0.], vec![0., 0., 1., 0.]],
-                            bi : vec![t, 2. * t, 2. * t, t],
-                            ci : vec![0., 0.5, 0.5, 1.],
-                        } 
-                    },
-                    NewtonButcherTableauBuilder::Heun(order) => {
-                        match order {
-                            2 => {
-                                ButcherTableau{
-                                    aij : vec![vec![0., 0.], vec![1., 0.]],
-                                    bi : vec![0.5, 0.5],
-                                    ci : vec![0., 1.],
-                                }
-                            },
-                            3 => {
-                                let t = 1. / 3.;
-                                ButcherTableau{
-                                    aij : vec![vec![0., 0., 0.], vec![t, 0., 0.], vec![0., 2. * t, 0.]],
-                                    bi : vec![0.25, 0., 0.75],
-                                    ci : vec![0., t, 2. * t],
-                                }
-                            }, 
-                            _ => {
-                                panic!("Heun methods exist only for 2nd and 3rd order");
-                            }
-                        }
-                    },
-                    NewtonButcherTableauBuilder::Ralston(order) => {
-                        match order {
-                            2 => {
-                                ButcherTableau{
-                                    aij : vec![vec![0., 0.], vec![2. / 3., 0.]],
-                                    bi : vec![0.25, 0.75],
-                                    ci : vec![0., 2. / 3.],
-                                }
-                            },
-                            3 => {
-                                let t = 1. / 9.;
-                                ButcherTableau{
-                                    aij : vec![vec![0., 0., 0.], vec![0.5, 0., 0.], vec![0., 0.75, 0.]],
-                                    bi : vec![2. * t, 3. * t, 4. * t],
-                                    ci : vec![0., 0.5, 0.75],
-                                }    
-                            }, 
-                            _ => {
-                                panic!("Raltson methods exist only for 2nd and 3rd order");
-                            }
-                        }
-                    },
-                    NewtonButcherTableauBuilder::Generic(order, alpha) => {
-                        match order {
-                            2 => {
-                                let t = 1. / (2. * alpha);
-                                ButcherTableau{
-                                    aij : vec![vec![0., 0.], vec![alpha, 0.]],
-                                    bi : vec![1. - t, t],
-                                    ci : vec![0., alpha],
-                                }
-                            },
-                            3 => {
-                                if alpha.abs_diff_eq(&0., 1e-5) || alpha.abs_diff_eq(&(2. / 3.), 1e-5) || alpha.abs_diff_eq(&1., 1e-5){
-                                    panic!("Generic 3rd order Runge-Kutta method cannot be made with alpha = 0, 2/3, 1");
-                                }
-                                let t = (1. - alpha) / (3. * alpha - 2.);
-                                ButcherTableau{
-                                    aij : vec![vec![0., 0., 0.], vec![alpha, 0., 0.], vec![1. + t / alpha, - t / alpha, 0.]],
-                                    bi : vec![0.5 - 1. / (6. * alpha), 1. / (6. * alpha * (1. - alpha)), - t / 6.],
-                                    ci : vec![0., alpha, 1.],
-                                }
-                            }, 
-                            _ => {
-                                panic!("Generic Runge-Kutta tableau exist only for 2nd and 3rd order");
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-impl_bt_builder!(f32);
-impl_bt_builder!(f64);
-
 
 
 /// Approximation method
@@ -188,7 +26,7 @@ pub struct NewtonRKIntegratorBuilder<T>{
 impl<T : Copy> NewtonRKIntegratorBuilder<T>{
     pub fn new() -> Self{
         Self{
-            name_tableau : None,
+            name_tableau : Some("Classic"),
             order_tableau : None,
             alpha_tableau : None,
         }
@@ -355,4 +193,6 @@ impl<T: Iterator + TimeIterator<f64>> Iterator for TimeDiffIterator<T> {
 pub mod langevin;
 pub mod newton;
 pub mod time;
+pub mod tableau;
 // pub mod brownian_dynamics;
+
